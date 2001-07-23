d1156ed62584bb54d89d33d87c8b738d7fc2c3e9
jjgarcia <jjgarcia>
2001-07-23 09:01:05 +0000

    Remove files which are no longer needed.

--- src/crs/porting/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int a[100];
-
-main()
-{
-	printf("Hello.\n");
-	printf("a[10] = %d\n", a[10]);
-}
--- src/crs/porting/o.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int a[100];
-
-extra_lenght_foo()
-{
-	printf("Hello.\n");
-	printf("a[10] = %d\n", a[10]);
-}
