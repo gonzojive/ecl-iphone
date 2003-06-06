ce0010c2dc16e378be1e23be15a1f2abe76aa507
jjgarcia <jjgarcia>
2003-06-06 16:18:59 +0000

    Some extra headers are required for select() in older systems.

--- src/c/file.d
+++ src/c/file.d
@@ -27,6 +27,9 @@
 
 #ifdef HAVE_SELECT
 #include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 #elif defined(BSD) && !defined(MSDOS)
 #include <sys/ioctl.h>
 #endif
