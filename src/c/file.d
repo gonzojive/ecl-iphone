2b8bfd6891818fd0ba0f3a3619fd81ee96f4588f
jjgarcia <jjgarcia>
2005-01-11 13:47:10 +0000

    MAKE-STRING-OUTPUT-STREAM now checks the validity of the :ELEMENT-TYPE.

--- src/c/file.d
+++ src/c/file.d
@@ -2395,6 +2395,10 @@ for the string ~S.",
 
 @(defun make-string-output-stream (&key (element_type @'base-char'))
 @
+	if (Null(cl_subtypep(element_type, @'character'))) {
+		FEerror("In MAKE-STRING-OUTPUT-STREAM, the argument :ELEMENT-TYPE (~A) must be a subtype of character",
+			1, element_type);
+	}
 	@(return make_string_output_stream(128))
 @)
 
