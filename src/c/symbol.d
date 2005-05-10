b0ec4469d2740c7b39303a01b568dbe2b1d0230e
jjgarcia <jjgarcia>
2005-05-10 14:14:37 +0000

    Changing the property list of NIL breaks ECL. Forbid it\!

--- src/c/symbol.d
+++ src/c/symbol.d
@@ -335,6 +335,9 @@ cl_object
 si_putprop(cl_object sym, cl_object value, cl_object indicator)
 {
 	assert_type_symbol(sym);
+	if (sym == Cnil) {
+	    FEerror("It is not allowed to change the property list of NIL.\nNote: This restriction is a known bug and will be eventually fixed.", 0);
+	}
 	sym->symbol.plist = si_put_f(sym->symbol.plist, value, indicator);
 	@(return value)
 }
