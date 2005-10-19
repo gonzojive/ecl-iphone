5d0c5ebafe5387f16944dcbf759e9557e9e619ef
goffioul <goffioul>
2005-10-19 08:03:02 +0000

    Mingw32 Typo.

--- src/c/time.d
+++ src/c/time.d
@@ -25,7 +25,7 @@
 #include <unistd.h>
 #endif
 
-#if defined(mignw32) || defined(_MSC_VER)
+#if defined(mingw32) || defined(_MSC_VER)
 /* The function sleep() in MinGW is bogus: it counts millisecons! */
 #include <windows.h>
 #define sleep(x) Sleep(x*1000)
