28f0b90e258811aa6620e4eea421384cb4794942
jjgarcia <jjgarcia>
2008-08-05 21:22:18 +0000

    Improved the code for using a smaller stack frame.

--- src/c/arch/apply_x86.d
+++ src/c/arch/apply_x86.d
@@ -24,17 +24,18 @@ APPLY(cl_narg n, cl_objectfn fn, cl_object *x)
 	"movl	4(%%ebp),%%edx\n\t"	/* Create a fake frame for debugger */
 	"pushl	%%edx\n\t"
 	"pushl	%%ebp\n\t"
-	"movl	%%ecx, %%edx\n\t"	/* Here we compute the new address of the stack pointer */
-	"movl	%%esp, %%ebp\n\t"	/* using the formula ESP = (ESP - ECX*4 - 4) & -16 */
-	"negl	%%edx\n\t"		/* which rounds ESP making it a multiple of 16 bytes. */
-	"leal	-4(%%esp,%%edx,4), %%esp\n\t"
-	"andl	$-16, %%esp\n\t"
-	"movl	%%ecx, (%%esp)\n\t"	/* Then ESP[0] is the number of arguments */
-	"leal	4(%%esp), %%edi\n\t"	/* and the other arguments are copied from ESP[4] on */
+	"movl	%%ecx, %%edx\n\t"
 	"cmpl	$63, %%ecx\n\t"		/* Copy at most 63 arguments onto the stack */
 	"jle	FOO1\n\t"
 	"movl	$63, %%ecx\n\t"
-"FOO1:\n"
+"FOO1:\n\t"				/* Here we compute the new address of the stack pointer */
+	"movl	%%esp, %%ebp\n\t"	/* using the formula ESP = (ESP - ECX*4 - 4) & -16 */
+	"negl	%%ecx\n\t"		/* which rounds ESP making it a multiple of 16 bytes. */
+	"leal	-4(%%esp,%%ecx,4), %%esp\n\t"
+	"andl	$-16, %%esp\n\t"
+	"movl	%%edx, (%%esp)\n\t"	/* Then ESP[0] is the number of arguments */
+	"negl	%%ecx\n\t"
+	"leal	4(%%esp), %%edi\n\t"	/* and the other arguments are copied from ESP[4] on */
 	"rep\n\t"
         "movsl\n\t"
 	"call	*%%eax\n\t"		/* At this point the stack must be aligned */
@@ -53,16 +54,17 @@ APPLY_fixed(cl_narg n, cl_object (*fn)(), cl_object *x)
 	"movl	4(%%ebp),%%edx\n\t"	/* Create a fake frame for debugger */
 	"pushl	%%edx\n\t"
 	"pushl	%%ebp\n\t"
-	"movl	%%ecx, %%edx\n\t"	/* Here we compute the new address of the stack pointer */
-	"movl	%%esp, %%ebp\n\t"	/* using the formula ESP = (ESP - ECX*4) & -16 */
-	"negl	%%edx\n\t"		/* which rounds ESP making it a multiple of 16 bytes. */
-	"leal	(%%esp,%%edx,4), %%esp\n\t"
-	"andl	$-16, %%esp\n\t"
-	"movl	%%esp, %%edi\n\t"	/* then the arguments are copied from ESP[0] on */
-	"cmpl	$63, %%ecx\n\t"		/* Copy at most 63 arguments onto the stack */
+	"movl	%%ecx, %%edx\n\t"	/* Copy at most 63 arguments onto the stack */
+	"cmpl	$63, %%ecx\n\t"
 	"jle	FOO2\n\t"
-	"movl	$63, %%ecx\n\t"
-"FOO2:\n"
+	"movl	$63, %%ecx\n"
+"FOO2:\n\t"				/* Here we compute the new address of the stack pointer */
+	"movl	%%esp, %%ebp\n\t"	/* using the formula ESP = (ESP - ECX*4) & -16 */
+	"negl	%%ecx\n\t"		/* which rounds ESP making it a multiple of 16 bytes. */
+	"leal	(%%esp,%%ecx,4), %%esp\n\t"
+	"andl	$-16, %%esp\n\t"
+	"negl	%%ecx\n\t"
+	"movl	%%esp, %%edi\n\t"	/* then the arguments are copied from ESP[0] on */
 	"rep\n\t"
         "movsl\n\t"
 	"call	*%%eax\n\t"		/* At this point the stack must be aligned */
@@ -81,18 +83,19 @@ APPLY_closure(cl_narg n, cl_objectfn fn, cl_object cl, cl_object *x)
 	"movl	4(%%ebp),%%edx\n\t"	/* Create a fake frame for debugger */
 	"pushl	%%edx\n\t"
 	"pushl	%%ebp\n\t"
-	"movl	%%ecx, %%edx\n\t"	/* Here we compute the new address of the stack pointer */
-	"movl	%%esp, %%ebp\n\t"	/* using the formula ESP = (ESP - ECX*4 - 8) & -16 */
-	"negl	%%edx\n\t"		/* which rounds ESP making it a multiple of 16 bytes. */
-	"leal	-8(%%esp,%%edx,4), %%esp\n\t"
-	"andl	$-16, %%esp\n\t"
-	"movl	%%ecx, (%%esp)\n\t"	/* Then ESP[0] is the number of arguments */
-	"movl	%%edi, 4(%%esp)\n\t"	/* ESP[4] is the closure environment */
-	"leal	8(%%esp), %%edi\n\t"	/* and the other arguments are copied from ESP[8] on */
+	"movl	%%ecx, %%edx\n\t"
 	"cmpl	$63, %%ecx\n\t"		/* Copy at most 63 arguments onto the stack */
 	"jle	FOO3\n\t"
 	"movl	$63, %%ecx\n\t"
-"FOO3:\n"
+"FOO3:\n\t"				/* Here we compute the new address of the stack pointer */
+	"movl	%%esp, %%ebp\n\t"	/* using the formula ESP = (ESP - ECX*4 - 8) & -16 */
+	"negl	%%ecx\n\t"		/* which rounds ESP making it a multiple of 16 bytes. */
+	"leal	-8(%%esp,%%ecx,4), %%esp\n\t"
+	"andl	$-16, %%esp\n\t"
+	"movl	%%edx, (%%esp)\n\t"	/* Then ESP[0] is the number of arguments */
+	"movl	%%edi, 4(%%esp)\n\t"	/* ESP[4] is the closure environment */
+	"negl	%%ecx\n\t"
+	"leal	8(%%esp), %%edi\n\t"	/* and the other arguments are copied from ESP[8] on */
 	"rep\n\t"
         "movsl\n\t"
 	"call	*%%eax\n\t"		/* At this point the stack must be aligned */
